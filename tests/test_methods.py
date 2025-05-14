78b9cba37a96c76b860d5ce0a262bdbf953a0b89
arvestad <arve@math.su.se>
2025-05-14 12:54:35 +0200

    first attempt at adding tests, to be good python citizen

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,17 +1,30 @@
 import unittest
+import statistics as stat
 
-import ark
+from ark import Ark, ArkRow
 
 class TestMethods(unittest.TestCase):
+    def setUp(self):
+        self.r1 = ArkRow([1,2,3])
+        self.r2 = ArkRow([3,2,1])
+        self.r1r2 = ArkRow([4,4,4])
+
+        self.ark = Ark([[1,2,3], [2,3,4],[3,4,5]], headers=['a', 'b', 'c'])
+        self.col_sum = ArkRow([6, 9, 12])
+        self.col_mean = ArkRow([2, 2, 2])
 
     def test_equal(self):
-        r1 = ArkRow([1,2,3])
-        assert r1 == r1
+        self.assertEqual(self.r1, self.r1)
         
 
+    def test_add(self):
+        self.assertEqual(self.r1 + self.r2, self.r1r2)
+
     def test_sum(self):
-        r1 = ArkRow([1,2,3])
-        r2 = ArkRow([3,2,1])
-        r1r2 = ArkRow([4,4,4])
-        assert r1 + r2 == r1r2
+        self.assertEqual(sum(self.ark), self.col_sum)
+
+    def test_mean(self):
+        self.assertEqual(stat.mean(self.ark), self.col_mean)
         
+if __name__ == '__main__':
+    unittest.main()
